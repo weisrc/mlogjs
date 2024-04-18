fcff1bd4b68734ca9aaecee88ab6430ffa4b63bf
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2024-04-18 11:18:52 -0300

    migrate most built-in functions to work with the new IR

--- compiler/src/macros/commands/SpawnWave.ts
+++ compiler/src/macros/commands/SpawnWave.ts
@@ -1,32 +1,37 @@
 import { CompilerError } from "../../CompilerError";
-import { InstructionBase } from "../../instructions";
+import { NativeInstruction } from "../../flow";
+import { nullId } from "../../utils";
 import { LiteralValue } from "../../values";
 import { MacroFunction } from "../Function";
 
 export class SpawnWave extends MacroFunction {
   constructor() {
     super((c, cursor, loc, ...args) => {
-
       const natural = c.getValue(args[0]);
+      const [, x, y] = args;
 
       if (
         !(natural instanceof LiteralValue) ||
         (natural.data !== 1 && natural.data !== 0)
       )
         throw new CompilerError(
-          "The 'natural' argument must be a boolean literal", loc
+          "The 'natural' argument must be a boolean literal",
+          loc,
         );
-      return [
-        null,
-        [
-          new InstructionBase(
-            "spawnwave",
-            x ?? "10",
-            y ?? "10",
-            String(!!natural.data),
-          ),
-        ],
-      ];
+
+      if (!x) throw new CompilerError("Missing argument: x", loc);
+      if (!y) throw new CompilerError("Missing argument: y", loc);
+
+      cursor.addInstruction(
+        new NativeInstruction(
+          ["spawnwave", x, y, String(!!natural.data)],
+          [x, y],
+          [],
+          loc,
+        ),
+      );
+
+      return nullId;
     });
   }
 }
